9a183bf6a9c02fb65f82faad351052f70d376bbb
고범석 <kobums@naver.com>
2026-02-11 11:09:00 +0900

    fix(main): 앱 초기화 시 Splash Screen 유지 로직 추가 (깜빡임 방지)

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:app/config/router.dart';
 import 'package:app/core/storage/storage_service.dart';
 import 'package:app/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   
   final prefs = await SharedPreferences.getInstance();
 
@@ -20,11 +22,23 @@ void main() async {
   );
 }
 
-class MyApp extends ConsumerWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    // 초기화가 완료되면 스플래시 화면 제거
+    FlutterNativeSplash.remove();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final router = ref.watch(routerProvider);
 
     return MaterialApp.router(
